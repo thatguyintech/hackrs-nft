3ff0de5fe7a8f27dabadb2eca4f1ba0dfbd9fc1e
ALBERT <thatguyintech@gmail.com>
2022-03-01 16:43:34 -0800

    mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.RINKEBY_PK],
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL,
+      accounts: [process.env.ROPSTEN_PK],
+    },
+    mainnet: {
+      url: process.env.MAINNET_URL,
+      accounts: [process.env.MAINNET_PK]
+    }
+  },
 };
